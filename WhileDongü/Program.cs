c93082c93004459e456f55f2fbdfce0bbbf2643e
Medeni Aba <medeniabax@gmail.com>
2023-11-09 14:23:57 +0300

    Proje dosyası ekle.

---
+++ WhileDongü/Program.cs
@@ -0,0 +1,16 @@
+﻿int carpim  = 1 ;
+int i = 1 ;
+int sayi;
+Console.WriteLine("Faktöriyel Degeri hesaplanacak sayıyı giriniz : ");
+
+sayi = int.Parse(Console.ReadLine());
+
+while ( i < sayi)
+{
+
+    carpim *= sayi;
+    
+    sayi--;
+   
+} 
+Console.WriteLine("Girdiniz Degerin Sonucu : " + carpim );
